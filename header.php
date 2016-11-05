73ea5beb0b7c04dae8bbde8d96bb96c8a69a5c65
Matheus Victor <hffmatheus@gmail.com>
2016-11-05 01:49:11 -0200

    adu=jmts

--- header.php
+++ header.php
@@ -48,29 +48,24 @@
             </div>
 
         </div>
-</div>
+    </div>
 
-<div class="wrapper">
-    <div class="wrapper-navigation">
-        <div class="container">
-            <div class="row">
-                <div class="menu-tabs">
-                    <ul class="nav nav-tabs">
-                        <li class="menu-item menu-active"><a class="menu-link">Início</a></li>
-                        <li class="menu-item"><a href="/pages/quartos.php" class="menu-link">Quartos</a></li>
-                        <li class="menu-item"><a class="menu-link">Como Chegar</a></li>
-                        <li class="menu-item"><a class="menu-link">Galeria</a></li>
-                        <li class="menu-item"><a class="menu-link">Reserve aqui</a></li>
-                        <li class="menu-item"><a class="menu-link">Serviços</a></li>
-                        <li class="menu-item"><a class="menu-link">Contato</a></li>
-                    </ul>
+    <div class="wrapper">
+        <div class="wrapper-navigation">
+            <div class="container">
+                <div class="row">
+                    <div class="menu-tabs">
+                        <ul class="nav nav-tabs">
+                            <li class="menu-item menu-active"><a class="menu-link">Início</a></li>
+                            <li class="menu-item"><a class="menu-link">Reservas</a></li>
+                            <li class="menu-item"><a href="/pages/quartos.php" class="menu-link">Quartos</a></li>
+                            <li class="menu-item"><a class="menu-link">Galeria</a></li>
+                            <li class="menu-item"><a class="menu-link">Serviços</a></li>
+                            <li class="menu-item"><a class="menu-link">Como Chegar</a></li>
+                            <li class="menu-item"><a class="menu-link">Contato</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="container wrapper-content">
-
-
-            </div>
-
-        </div>
+            <div class="container wrapper-content">
--- pages/quartos.php
+++ pages/quartos.php
@@ -3,6 +3,46 @@ require_once '../libs/config.php';
 
 get_header('Quartos');
 
+$quartosAll = array(
+    'Jurere' => array('jurere1.jpg', 'jurere2.jpg', 'jurere3.jpg', 'jurere4.jpg'),
+    'Matadeiro' => array('matadouro1.jpg', 'matadouro2.jpg', 'matadouro3.jpg'),
+    'Praia Mole' => array('praia_mole1.jpg', 'praia_mole2.jpg'),
+    'Joaquina' => array('joaquina1.jpg'),
+    'Costão do Santinho' => array('costaoDoSantinho1.jpg', 'costaoDoSantinho2.jpg', 'costaoDoSantinho3.jpg', 'costaoDoSantinho4.jpg')
+);
+
+$quartos = array(
+    'Jurere' => array('jurere1.jpg'),
+    'Matadeiro' => array('matadouro1.jpg'),
+    'Praia Mole' => array('praia_mole1.jpg'),
+    'Joaquina' => array('joaquina1.jpg'),
+    'Costão do Santinho' => array('costaoDoSantinho1.jpg')
+);
+
 ?>
 
+<div class="row">
 
+    <?php foreach ($quartos as $quarto => $imagens) { ?>
+
+        <?php foreach ($imagens as $img) { ?>
+
+            <div class="col-lg-12 text-center section-rooms">
+
+            </div>
+
+            <div class="col-lg-4">
+                <img src="/assets/img/<?= $img ?>" class="img-responsive img-rounded">
+            </div>
+
+            <div class="col-lg-8">
+                <h3 class="text-center">Quarto <?= $quarto ?> </h3>
+            </div>
+
+        <?php } ?>
+
+    <?php } ?>
+</div>
+
+
+</div>
