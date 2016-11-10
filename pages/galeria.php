0ec94c098cd475680edd8fed9c0a6ef304477c20
Matheus Victor <hffmatheus@gmail.com>
2016-11-10 11:28:18 -0200

    adjustments pages

--- pages/galeria.php
+++ pages/galeria.php
@@ -3,28 +3,43 @@ require_once '../libs/config.php';
 
 get_header('Galeria de Fotos');
 
+$imgCidade = glob('../assets/img/cidade/*.*');
+$imgHostel = glob('../assets/img/hostel/*.*');
+$imgPraias = glob('../assets/img/praias/*.*');
+
+$arrayImagens = array(
+    "A cidade" => glob('../assets/img/cidade/*.*'),
+    "O hostel" => glob('../assets/img/hostel/*.*'),
+    "Praias" => glob('../assets/img/praias/*.*')
+);
 ?>
 
-<div class="container">
-    <div class="row section-gallery">
-        
-        <div class="col-lg-4 collum-gallery">
-            <img src="/assets/img/intro1.jpg" class="img-responsive img-circle">
-            <h4 class="text-center">A cidade</h4>
-        </div>
+    <link rel="stylesheet" href="/assets/css/galeria.css">
 
-        <div class="col-lg-4 collum-gallery">
-            <img src="/assets/img/frente5.jpg" class="img-responsive img-circle">
-            <h4 class="text-center">O hostel</h4>
-        </div>
+    <div class="container">
+        <div class="row section-gallery">
 
-        <div class="col-lg-4 collum-gallery">
-            <img src="/assets/img/joaquina.jpg" class="img-responsive img-circle">
-            <h4 class="text-center">Praias</h4>
-        </div>
+            <?php foreach ($arrayImagens as $titulo => $imagens) { ?>
 
+                <div class="col-lg-12">
+                    <h2 class="text-center"><?= $titulo ?></h2>
+                    <br>
+                </div>
+
+                <?php foreach ($imagens as $img) { ?>
+
+                    <div class="col-lg-4 collum-gallery">
+                        <img src="<?= $img ?>" class="img-responsive img-circle">
+                    </div>
+
+                <?php } ?>
+
+                <br>
+
+            <?php } ?>
+
+        </div>
     </div>
-</div>
 
 
 <?php
